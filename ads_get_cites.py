ce25eb4641cb297917be4de3208969bcadec6558
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-05-01 14:42:24 -0700

    Add exeption for HTTPError

--- ads_get_cites.py
+++ ads_get_cites.py
@@ -92,7 +92,10 @@ for library in args:
 
         # get publication date
         url = 'http://adsabs.harvard.edu/abs/%s' % bibcode
-        u = urllib2.urlopen(url)
+        try:
+            u = urllib2.urlopen(url)
+        except urllib2.HTTPError:
+            continue
         lines = u.readlines()
         for line in lines:
             if line.startswith('<tr>') and 'Publication Date' in line:
